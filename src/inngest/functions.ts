dc4be15bbebfe558b7d82e4900b46ce7cd6d8064
Tushar4440 <tushartewari33@gmail.com>
2025-12-20 16:17:37 +0530

    - : Integrated Inngest client and functions for event handling. - : Set up Prisma client for database interactions. - :Established TRPC setup for API communication with query client and routers.

---
+++ src/inngest/functions.ts
@@ -0,0 +1,20 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+    console.log(output);
+    return { output };
+  },
+);
