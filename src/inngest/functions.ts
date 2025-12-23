335b78f193879b7d970a9790a0ea69e0b5fff5c1
Tushar4440 <tushartewari33@gmail.com>
2025-12-23 09:50:35 +0530

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,20 +1,34 @@
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-tushar");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer. You write readable, maintable code. You write simple Next.js & React snippets.",
       model: openai({ model: "gpt-4o" }),
     });
+    
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`,
     );
-    console.log(output);
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async ()=>{
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { output, sandboxUrl };
   },
 );
